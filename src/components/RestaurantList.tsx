1a3b0886833a88bb247eff0c117e0d490c18b91b
Matthew Burfield <m1.burfield@gmail.com>
2022-02-09 07:24:58 +1000

    Displays restaurants in RestaurantList

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,16 +1,27 @@
 import * as React from 'react';
 
+type tRestaurant = {
+  id: number;
+  name: string;
+};
+
 interface Props {
   loadRestaurants: () => void;
+  restaurants: tRestaurant[];
 }
 
 export default function RestaurantList(props: Props) {
-
   React.useEffect(() => {
     props.loadRestaurants();
   }, [props.loadRestaurants]);
 
-  return <div>RestaurantList</div>;
+  return (
+    <ul>
+{props.restaurants.map((restaurant) => (
+        <li key={restaurant.id}>{restaurant.name}</li>
+      ))}
+</ul>
+  );
 }
 
 export { RestaurantList };
--- src/components/__tests__/RestaurantList.spec.tsx
+++ src/components/__tests__/RestaurantList.spec.tsx
@@ -5,8 +5,23 @@ describe('RestaurantList', () => {
   it('loads restaurants on the first render', () => {
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
 
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    render(<RestaurantList loadRestaurants={loadRestaurants} restaurants={[]} />);
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('should display the restaurants', () => {
+    const noop = () => void 0;
+    const restaurants = [
+      { id: 1, name: 'Sushi Place' },
+      { id: 2, name: 'Pizza Place' }
+    ];
+
+    const { queryByText } = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />
+    );
+
+    expect(queryByText('Sushi Place')).not.toBeNull();
+    expect(queryByText('Pizza Place')).not.toBeNull();
   });
 });
