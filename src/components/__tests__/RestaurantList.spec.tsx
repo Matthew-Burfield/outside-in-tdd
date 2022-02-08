9990096cdb4924ea2d0c1e82dd8e1c1e5c0f554c
Matthew Burfield <m1.burfield@gmail.com>
2022-02-09 07:40:06 +1000

    refactor

--- src/components/__tests__/RestaurantList.spec.tsx
+++ src/components/__tests__/RestaurantList.spec.tsx
@@ -1,25 +1,31 @@
 import { render } from '@testing-library/react';
+import type { RenderResult } from '@testing-library/react';
 import { RestaurantList } from '../RestaurantList';
 
 describe('RestaurantList', () => {
+  const restaurants = [
+    { id: 1, name: 'Sushi Place' },
+    { id: 2, name: 'Pizza Place' }
+  ];
+  let loadRestaurants: jest.Mock;
+  let context: RenderResult;
+
+  beforeEach(() => {
+    loadRestaurants = jest.fn().mockName('loadRestaurants');
+    context = render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />
+    );
+  });
+
   it('loads restaurants on the first render', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-
-    render(<RestaurantList loadRestaurants={loadRestaurants} restaurants={[]} />);
-
     expect(loadRestaurants).toHaveBeenCalled();
   });
 
   it('should display the restaurants', () => {
-    const noop = () => void 0;
-    const restaurants = [
-      { id: 1, name: 'Sushi Place' },
-      { id: 2, name: 'Pizza Place' }
-    ];
-
-    const { queryByText } = render(
-      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />
-    );
+    const { queryByText } = context;
 
     expect(queryByText('Sushi Place')).not.toBeNull();
     expect(queryByText('Pizza Place')).not.toBeNull();
