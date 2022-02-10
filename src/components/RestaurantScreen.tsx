9d565e4df67ef245cbce70db0547c7d996338f1e
Matthew Burfield <m1.burfield@gmail.com>
2022-02-10 18:38:12 +1000

    Style updates

--- src/components/RestaurantScreen.tsx
+++ src/components/RestaurantScreen.tsx
@@ -1,10 +1,13 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
 import RestaurantList from './RestaurantList';
 
 export default function RestaurantScreen() {
   return (
-    <div>
-      <h1>Restaurants</h1>
-      <RestaurantList />
-    </div>
+    <Card>
+      <CardContent>
+        <Typography variant="h5">Restaurants</Typography>
+        <RestaurantList />
+      </CardContent>
+    </Card>
   );
 }
